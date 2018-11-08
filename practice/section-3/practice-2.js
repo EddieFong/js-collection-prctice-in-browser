8f7af5a40344fe2c5b03ab39f5b2c75522c3a37c
Eddie Fong <fonged@oocl.com>
2018-11-08 17:22:32 +0800

    remove typo

--- practice/section-3/practice-2.js
+++ practice/section-3/practice-2.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  countSameElements(collectionA);
+  
   objectB.value.forEach((BItem) => {
     var foundIndex = collectionA.findIndex(x => x.key == BItem);
     collectionA[foundIndex].count = collectionA[foundIndex].count - Math.floor( collectionA[foundIndex].count / 3);
